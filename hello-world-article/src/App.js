c35a355a606cab0fb442aec9160a70e3f7d24ed6
merlemak <merlemakoben@gmail.com>
2022-11-15 15:29:43 +0000

    add solution to challenges

--- hello-world-article/src/App.js
+++ hello-world-article/src/App.js
@@ -1,6 +1,15 @@
-import React from "react";
 import "./styles.css";
 
+function HelloWorldArticle() {
+  return (
+    <article>
+      <h1>Hello World!</h1>
+      <p>This is my first React component</p>
+      <p>... and it is beautiful!</p>
+    </article>
+  );
+}
+
 export default function App() {
-  return <div>Replace me with your Component!</div>;
+  return <HelloWorldArticle />;
 }
